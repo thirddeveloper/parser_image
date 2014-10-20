7aa56f4decf8bf9f8859f9eea89a985cac67301a
Maxim Y <arantar22@gmail.com>
2014-10-20 12:15:57 -0700

    require 'net/http' require 'nokogiri' require 'open-uri' require 'openssl'
    #параметры скрипта:
    # 1 п. - адрес сайта
    # 2 п. - асолютный путь для загрузки картинок
    params = Array.new(2)
    if ARGV.size == params.size
    	params = ARGV.each {|a|}
    
    	content = open(params[0], ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, 'User-Agent' => 'opera')
    	doc = Nokogiri::HTML(content)
    	doc_body = doc.css("img")
    
    	#код для сохранения названия картинок
    	arr_str = []
    	for i in 0..doc_body.length - 1
    		j = doc_body[i]['src'].length
    		str = ""
    		while j >= 0
    			if doc_body[i]['src'][j] != '/'
    				str += doc_body[i]['src'][j].to_s
    			else
    				break
    			end
    			j -= 1
    		end
    		arr_str[i] =  "(" + i.to_s + ") "+ str.reverse
    	end
    	#конец
    
    	t1 = Time.now
    	#подключение к серверу, создание и запись данных в файл
    	uri = URI(params[0])
    	threads = []
    	resp = []
    	for a in 0..doc_body.length - 2
    		#p a.to_s + ". " + doc_body[a]['src']
    		if doc_body[a]['src'] != ""
    			#p 1
    			threads << Thread.new(doc_body[a]['src']) do |url|
    				#p 2
    				http = Net::HTTP::new(uri.host, uri.port)
    				resp[a] = http.get(url)
    				#if resp[a].message == "OK"
    					open(params[1] + arr_str[a], "wb") do |file|
    			    		file.write(resp[a].body)
    					end
    				#else
    					#p resp[a].message
    					#next
    				#end
    				#puts "Got #{doc_body[a]['src']}: #{resp.message}"
    			end
    			#sleep(0.05)
    			#threads[a].join
    		end
    	end
    	threads.each { |thr| thr.join }
    
    	t2 = Time.now
    	p t2 - t1
    else
    	puts "Вы не ввели необходимое кол-во параметров!"
    end

---
+++ grab.rb
@@ -0,0 +1,47 @@
+require 'open-uri'
+require 'nokogiri'
+ 
+unless ARGV.length == 2
+    puts "Инструкция: {имя_скрипта, url-адрес_сайта целевая_папка}"
+else
+    url=ARGV[0]
+    path=ARGV[1]
+    data=Nokogiri::HTML(open(url))
+    imgtags=data.css("img")
+    arrImages=Array.new()
+    file_extension=String.new
+    counter=1
+    imgtags.each do |tagtxt|
+        val=tagtxt['src']
+        filename="img_"
+        unless val.nil?
+            if val.rindex('.').nil?
+                file_extension='.jpg'
+            else
+                server_ext=val[val.rindex('.'),val.length]
+                if server_ext.length>4
+                    file_extension='.jpg'
+                else
+                    file_extension=val[val.rindex('.'),val.length]
+                end
+            end
+                while File.exists?(path+filename+counter.to_s+file_extension)
+                    counter+=1
+                end
+             
+            if val.rindex("http:").nil? && val.rindex("https:").nil?
+                if val[0,2]=="//"
+                    val="http:"+val
+                elsif val[0,1]=="/"
+                    val="http:/"+val
+                else
+                    val="http://"+val
+                end
+            end
+            File.open(path+""+filename+counter.to_s+file_extension.to_s,"wb") do |f|
+                f << open(val).read
+            end
+        end
+        counter+=1
+    end
+end
---
+++ pars.rb
@@ -0,0 +1,67 @@
+require 'net/http'
+require 'nokogiri'
+require 'open-uri'
+require 'openssl'
+
+#параметры скрипта:
+# 1 п. - адрес сайта 
+# 2 п. - асолютный путь для загрузки картинок
+params = Array.new(2)
+if ARGV.size == params.size
+	params = ARGV.each {|a|}
+
+	content = open(params[0], ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, 'User-Agent' => 'opera')
+	doc = Nokogiri::HTML(content)
+	doc_body = doc.css("img")
+	
+	#код для сохранения названия картинок
+	arr_str = []
+	for i in 0..doc_body.length - 1
+		j = doc_body[i]['src'].length
+		str = ""
+		while j >= 0
+			if doc_body[i]['src'][j] != '/'
+				str += doc_body[i]['src'][j].to_s
+			else	
+				break
+			end
+			j -= 1
+		end
+		arr_str[i] =  "(" + i.to_s + ") "+ str.reverse
+	end
+	#конец
+
+	t1 = Time.now
+	#подключение к серверу, создание и запись данных в файл
+	uri = URI(params[0])
+	threads = []
+	resp = []
+	for a in 0..doc_body.length - 2
+		#p a.to_s + ". " + doc_body[a]['src']
+		if doc_body[a]['src'] != ""
+			#p 1
+			threads << Thread.new(doc_body[a]['src']) do |url|
+				#p 2
+				http = Net::HTTP::new(uri.host, uri.port)
+				resp[a] = http.get(url)
+				#if resp[a].message == "OK"
+					open(params[1] + arr_str[a], "wb") do |file|
+			    		file.write(resp[a].body)
+					end
+				#else
+					#p resp[a].message
+					#next
+				#end
+				#puts "Got #{doc_body[a]['src']}: #{resp.message}"
+			end
+			#sleep(0.05)
+			#threads[a].join
+		end
+	end
+	threads.each { |thr| thr.join }
+
+	t2 = Time.now
+	p t2 - t1
+else
+	puts "Вы не ввели необходимое кол-во параметров!"
+end
